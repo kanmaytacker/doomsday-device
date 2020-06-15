db8d441c543c6a8ec745eafba0b1d29a6dff3d38
kanmaytacker <tanmay@locus.sh>
2020-06-15 17:10:22 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='doomsday-device',
+    version='0.1.0',
+    packages=[''],
+    url='www.locus.sh',
+    license='MIT',
+    author='tanmay',
+    author_email='tanmay@locus.sh',
+    description='Dr.Strangelove'
+)
